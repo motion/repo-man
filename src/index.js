9b4137123fd00e6dfe527e7713d44c855bc143d4
natew <natewienert@gmail.com>
2017-02-05 20:20:19 -0800

    :art: naming / working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import command from 'sb-command'
 import Path from 'path'
 import RepoMan from './repoman'
@@ -8,11 +10,10 @@ command
   .version('0.0.1')
   .description('Manage your repos')
   .command('init', 'Sets up your root ~/.repoman config file', RepoManager.init)
-  .command('import <repo>', 'Import repo to repoman', RepoManager.import)
-  .command('track [repos...]', 'Add repo to repoman', RepoManager.track)
-  .command('untrack [repos...]', 'Remove repo from repoman', RepoManager.untrack)
+  .command('add [repos...]', 'Add repo to repoman', RepoManager.track)
+  .command('remove [repos...]', 'Remove repo from repoman', RepoManager.untrack)
+  .command('publish [repos...]', 'Release new versions', RepoManager.release)
   .command('status', 'Get status of repos', RepoManager.status)
-  .command('release', 'Release new versions', RepoManager.release)
-  .command('run <command>', 'Runs command for every repo, if exists in package.json', RepoManager.run)
+  .command('exec', 'Exec shell command in every repo', RepoManager.run)
   .default(RepoManager.status)
   .parse(process.argv)
--- src/repoman.js
+++ src/repoman.js
@@ -1,4 +1,28 @@
 export default class RepoMan {
   constructor() {
   }
+
+  init(options, repos) {
+    console.log(options, repos)
+  }
+
+  add(options, repos) {
+    console.log(options, repos)
+  }
+
+  remove(options, repos) {
+    console.log(options, repos)
+  }
+
+  status(options, repos) {
+    console.log(options, repos)
+  }
+
+  publish(options, repos) {
+    console.log(options, repos)
+  }
+
+  exec(options, repos) {
+    console.log(options, repos)
+  }
 }
