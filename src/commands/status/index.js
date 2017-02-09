e7829b7ffbf104357147edb7f29ad9b174b4ec1e
Steel Brain <i@steelbrain.me>
2017-02-09 07:32:02 +0500

    :new: Make the status command work

--- src/commands/status/index.js
+++ src/commands/status/index.js
@@ -1,9 +1,19 @@
 // @flow
 
+import Table from 'cli-table'
+
 export const name = 'status'
 export const description = 'Get status of your projects'
 export async function callback() {
   const projectPaths = await this.getProjects()
   const projects = await Promise.all(projectPaths.map(entry => this.getProjectDetails(entry)))
-  console.log('projects', projects)
+
+  const table = new Table({
+    head: ['name', 'path', 'branch', 'changes'],
+  })
+  table.push(...projects.map(function(project) {
+    return [project.name, project.path, project.repository.branch, project.repository.filesDirty + project.repository.filesUntracked]
+  }))
+
+  this.log(table.toString())
 }
