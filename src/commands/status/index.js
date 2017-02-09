16f743c135c86440946c5e83c87f593a38386878
natew <natewienert@gmail.com>
2017-02-08 21:52:54 -0800

    :art: nicer

--- src/commands/status/index.js
+++ src/commands/status/index.js
@@ -7,19 +7,26 @@ export async function callback() {
   const projects = await Promise.all(projectPaths.map(entry => this.getProjectDetails(entry)))
   const { Table, Color, Figure, Symbol } = this.utils
 
-  const head = ['project', 'changes', `local ${Figure.arrowRight} remote`, 'npm', 'path'].map(c => Color.cyan(c))
+  const head = [
+    '  project',
+    'changes',
+    'branch',
+    'npm',
+    'path',
+  ].map(c => Color.xterm(247)(c))
+
   const table = new Table({ head })
+  const gray = Color.xterm(8)
 
   table.push(...projects.map(function(project) {
     const repo = project.repository
     const DIRTY_FLAG = repo.clean ? Symbol.check : Symbol.x
-
     return [
       `${DIRTY_FLAG} ${project.name}`,
       repo.filesDirty + repo.filesUntracked,
-      `${Color.yellow(repo.localBranch)} ${Figure.arrowRight} ${repo.remoteBranch}`,
-      project.package.version || '-none-',
-      Color.xterm(8)(project.path),
+      `${Color.yellow(repo.localBranch)} ${gray(Figure.arrowRight)} ${repo.remoteBranch}`,
+      project.package.version || gray('-none-'),
+      gray(project.path),
     ]
   }))
 
