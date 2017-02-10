6a539f7b8832186b3ab295627a8ad506c006c031
natew <natewienert@gmail.com>
2017-02-10 02:46:06 -0800

    :bug: fix

--- src/commands/EjectCommand.js
+++ src/commands/EjectCommand.js
@@ -32,39 +32,31 @@ export default class EjectCommand extends Command {
     // prompt for org to eject to
     const projectsPath = this.getProjectsRoot()
     const prefixPath = tildify(projectsPath)
-    const orgOpts = orgs.map(({ name, path }) => ({ name: `${new Array(prefixPath.length + 2).fill().join(' ')}${name}`, value: path }))
-    const answer = await prompt(`${prefixPath}/_____/${sourceName}`, orgOpts)
-
+    const orgOpts = orgs.map(({ name, path }) => ({
+      name: `${new Array(prefixPath.length + 2).fill().join(' ')}${name}`,
+      value: path,
+    }))
+    const answerOrg = await prompt(`${prefixPath}/_____/${sourceName}`, orgOpts)
     this.newline()
-
     let finalConfig = config
     if (!config) {
-      this.log('Config source? (any git repo, or Github shorthand)')
+      this.log('Config source? (git url or github/repo)')
       finalConfig = await prompt.input(':')
     }
 
-    const org = orgs[orgs.findIndex(x => x.path === answer)]
+    const org = orgs[orgs.findIndex(x => x.path === answerOrg)]
     const targetDir = Path.join(org.path, sourceName)
-    const targetExists = await FS.exists(targetDir)
 
-    if (!targetExists) {
-      await copy(sourceDir, targetDir)
-    } else {
-      const val = await prompt.input('overwrite? y/n')
-      if (val === 'y') {
-        await FS.rename(sourceDir, targetDir)
-      }
-      else {
-        this.log(Color.brightBlack(`Skipping ${tildify(targetDir)}`))
-      }
+    if (await FS.exists(targetDir)) {
+      this.log(Color.brightBlack(`Skipping ${tildify(targetDir)}`))
+      return
     }
-
+    await copy(sourceDir, targetDir)
     // add config
     if (finalConfig) {
       const configFile = new ConfigFile(Path.join(targetDir, Helpers.CONFIG_FILE_NAME))
       configFile.set('configurations', [finalConfig])
     }
-
     this.log(`${Color.white(tildify(sourceDir))} ${Figure.rightArrow} ${Color.yellow.bold(tildify(targetDir))}`)
   }
 }
