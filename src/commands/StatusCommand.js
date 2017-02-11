ef53a4da961b7c3e1a2d829237f0d664a2664f40
natew <natewienert@gmail.com>
2017-02-10 16:20:47 -0800

    :new: npm status

--- src/commands/StatusCommand.js
+++ src/commands/StatusCommand.js
@@ -1,60 +1,86 @@
 // @flow
 import Command from '../command'
+import type { Project } from '../types'
+const { all } = Promise
 
 export default class StatusCommand extends Command {
   name = 'status'
   description = 'Get status of your projects'
 
-  async run({ npm }) {
+  async run(_) {
     const { Table, Color, Figure, Symbol, tildify } = this.utils
 
     const projectPaths = await this.getProjects()
-    const projects = await Promise.all(
-      projectPaths.map(entry =>
-        this.getProjectDetails(entry, npm)
-      )
+    const showNpm = !!Object.keys(_).filter(x => x === 'npm').length
+    const projects = await all(
+      projectPaths.map(entry => this.getProjectDetails(entry, showNpm))
     )
 
-    let head = [
-      '  project', 'changes', 'branch', 'path',
+    const head = [
+      '  project',
+      'changes',
+      'branch',
+      showNpm && 'npm',
+      'path',
     ]
-    if (npm) {
-      head.push('npm version')
-    }
-    head = head.map(Color.xterm(247))
+      .filter(x => !!x)
+      .map(c => Color.xterm(247)(c))
 
-    const table = new Table({ head })
+
+    const { min, round } = Math
+    const getWidth = () =>
+      min(30, round((process.stdout.columns / head.length) * 0.9))
+    const colWidths = head.map(getWidth)
+    const table = new Table({ head, colWidths })
     const gray = Color.xterm(8)
 
-    table.push(...projects.map(function(project) {
-      const repo = project.repository
-      const isGit = typeof repo.clean !== 'undefined'
-      const none = gray('-none-')
-      const version = project.version || none
-      const path = gray(tildify(project.path))
+    // preview
+    const preview = new Table({ head, colWidths })
+    const rows = await all(projects.map(() => this.getRow()))
+    rows.forEach(r => preview.push(r))
+    this.log(preview.print())
 
-      if (isGit) {
-        const isDirty = repo.clean ? Symbol.check : Symbol.x
-        const numChanged = repo.filesDirty + repo.filesUntracked
-        return [
-          `${isDirty} ${project.name}`,
-          numChanged,
-          `${Color.yellow(repo.branchLocal)} ${gray(Figure.arrowRight)} ${repo.branchRemote}`,
-          path,
-          version,
-        ]
-      }
-      else {
-        return [
-          `  ${project.name}`,
-          none,
-          none,
-          version,
-          path,
-        ]
-      }
-    }))
-
+    // response
+    const final = await all(projects.map(this.getRow))
+    final.forEach(r => table.push(r))
     this.log(table.print())
+  }
+
+  getRow = async (project:? Project) => {
+    const gray = Color.xterm(8)
+
+    if (!project) {
+      projects.length
+    }
+
+    const repo = project.repository
+    const isGit = typeof repo.clean !== 'undefined'
+    const none = gray('  -  ')
+    const path = gray(tildify(project.path))
+
+    let response
+    const version = showNpm ? project.version || none : none
+
+    if (isGit) {
+      const isDirty = repo.clean ? Symbol.check : Symbol.x
+      const numChanged = repo.filesDirty + repo.filesUntracked
+      response = [
+        `${isDirty} ${project.name}`,
+        numChanged || none,
+        `${Color.yellow(repo.branchLocal)} ${gray(Figure.arrowRight)} ${repo.branchRemote}`,
+        version,
+        path,
+      ]
+    }
+    else {
+      response = [
+        `  ${project.name}`,
+        none,
+        none,
+        version,
+        path,
+      ]
+    }
+    return response.filter(x => !!x)
   }
 }
