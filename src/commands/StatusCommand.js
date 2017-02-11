5c260345de648a23dcc8d66743bb39d78a4a8305
natew <natewienert@gmail.com>
2017-02-10 16:24:04 -0800

    :bug: fix late night bad code

--- src/commands/StatusCommand.js
+++ src/commands/StatusCommand.js
@@ -1,65 +1,52 @@
 // @flow
 import Command from '../command'
 import type { Project } from '../types'
-const { all } = Promise
 
 export default class StatusCommand extends Command {
   name = 'status'
   description = 'Get status of your projects'
 
   async run(_) {
-    const { Table, Color, Figure, Symbol, tildify } = this.utils
+    this.showNpm = !!Object.keys(_).filter(x => x === 'npm').length
 
     const projectPaths = await this.getProjects()
-    const showNpm = !!Object.keys(_).filter(x => x === 'npm').length
-    const projects = await all(
-      projectPaths.map(entry => this.getProjectDetails(entry, showNpm))
+    const projects = await Promise.all(
+      projectPaths.map(entry => this.getProjectDetails(entry, this.showNpm))
     )
 
     const head = [
       '  project',
       'changes',
       'branch',
-      showNpm && 'npm',
+      this.showNpm && 'npm',
       'path',
     ]
       .filter(x => !!x)
-      .map(c => Color.xterm(247)(c))
-
+      .map(c => this.utils.Color.xterm(247)(c))
 
     const { min, round } = Math
     const getWidth = () =>
       min(30, round((process.stdout.columns / head.length) * 0.9))
     const colWidths = head.map(getWidth)
-    const table = new Table({ head, colWidths })
-    const gray = Color.xterm(8)
-
-    // preview
-    const preview = new Table({ head, colWidths })
-    const rows = await all(projects.map(() => this.getRow()))
-    rows.forEach(r => preview.push(r))
-    this.log(preview.print())
+    const table = new this.utils.Table({ head, colWidths })
 
     // response
-    const final = await all(projects.map(this.getRow))
+    const final = await Promise.all(projects.map(this.getRow))
     final.forEach(r => table.push(r))
     this.log(table.print())
   }
 
   getRow = async (project:? Project) => {
+    const { Color, Figure, Symbol, tildify } = this.utils
+
     const gray = Color.xterm(8)
-
-    if (!project) {
-      projects.length
-    }
-
     const repo = project.repository
     const isGit = typeof repo.clean !== 'undefined'
     const none = gray('  -  ')
     const path = gray(tildify(project.path))
 
     let response
-    const version = showNpm ? project.version || none : none
+    const version = this.showNpm ? project.version || none : false
 
     if (isGit) {
       const isDirty = repo.clean ? Symbol.check : Symbol.x
