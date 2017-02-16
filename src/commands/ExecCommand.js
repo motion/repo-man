ed82449709233ea0a6d32e427901aeb37b6594d0
Steel Brain <i@steelbrain.me>
2017-02-17 04:08:02 +0500

    :art: Make repoman exec work in all projects

--- src/commands/ExecCommand.js
+++ src/commands/ExecCommand.js
@@ -4,14 +4,16 @@ import Command from '../command'
 
 export default class InitCommand extends Command {
   name = 'exec <command> [parameters...]'
-  description = 'Run command in a set of repos'
+  description = 'Run command in all repos'
 
   // TODO: Remove variadic on parameters when sb-command returns it as an array by default
   async run(_: Object, command: string, ...parameters: Array<string>) {
-    const currentProjectPath = await this.getCurrentProjectPath()
-    await this.spawn(command, parameters, {
-      cwd: currentProjectPath,
-      stdio: ['inherit', 'inherit', 'inherit'],
-    })
+    const projects = await this.getProjects()
+    for (const project of projects) {
+      await this.spawn(command, parameters, {
+        cwd: project,
+        stdio: ['inherit', 'inherit', 'inherit'],
+      })
+    }
   }
 }
