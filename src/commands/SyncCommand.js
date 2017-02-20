217098b03bddc50fd58bde10979f7c1afa66389c
natew <natewienert@gmail.com>
2017-02-19 16:40:45 -0800

    :new: fast sync configs

--- src/commands/SyncCommand.js
+++ src/commands/SyncCommand.js
@@ -1,3 +1,4 @@
+
 // @flow
 
 import FS from 'sb-fs'
@@ -5,25 +6,31 @@ import Path from 'path'
 import copy from 'sb-copy'
 import invariant from 'assert'
 import { Observable } from 'rxjs/Observable'
-
 import Command from '../command'
 import { parseSourceURI } from '../helpers'
+import { uniq, flatten } from 'lodash'
 
 export default class SyncCommand extends Command {
   name = 'sync [org]'
   description = 'Sync configuration for projects, defaults to just current folder'
 
-  async run(options: Object, orgName: string) {
+  run = async (options: Object, orgName: string) => {
     const projectPaths = orgName ? await this.getProjects(orgName) : [await this.getCurrentProjectPath()]
     const overwrite = await this.helpers.prompt('Overwrite files on conflict?', ['no', 'yes']) === 'yes'
     const projects = await Promise.all(projectPaths.map(path => this.getProjectDetails(path)))
-
     const commandGet = this.repoMan.getCommand('get')
     const commandGetConfig = this.repoMan.getCommand('get-config')
     const projectsRoot = await this.getProjectsRoot()
     invariant(commandGet, 'get command not found when syncing repo')
     invariant(commandGetConfig, 'get-config command not found when syncing repo')
 
+    // update all configuration repos
+    const configs = uniq(flatten(projects.map(project => project.configurations)))
+    await Promise.all(
+      configs.map(config => commandGetConfig.run({}, config))
+    )
+
+    // copy configs
     await this.helpers.parallel('Syncing Projects', projects.map(project => ({
       title: project.name,
       // eslint-disable-next-line
@@ -40,7 +47,7 @@ export default class SyncCommand extends Command {
             }
           } catch (error) {
             observer.complete()
-            observer.throw(error)
+            this.log(error)
             return
           }
         }
@@ -49,9 +56,7 @@ export default class SyncCommand extends Command {
         for (const entry of project.configurations) {
           try {
             const configPath = this.getConfigPath(parseSourceURI(entry))
-            if (!await FS.exists(configPath)) {
-              await commandGetConfig.run({ silent: true }, entry)
-            }
+            // this.log(`Copying ${configPath} ${this.helpers.Figure.arrowRight} ${project.path}`)
             await copy(configPath, project.path, {
               filter: source => Path.basename(source) !== '.git',
               dotFiles: true,
@@ -60,7 +65,7 @@ export default class SyncCommand extends Command {
             })
           } catch (error) {
             observer.complete()
-            observer.throw(error)
+            this.log(error)
             return
           }
         }
