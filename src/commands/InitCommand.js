873ca3561a53ab3a90dbae95d6b5edc35435ce09
Steel Brain <i@steelbrain.me>
2017-02-21 05:56:35 +0500

    :new: Make repoman init work from subdirs of a project

--- src/commands/InitCommand.js
+++ src/commands/InitCommand.js
@@ -1,8 +1,8 @@
 // @flow
 
+import FS from 'sb-fs'
 import Path from 'path'
 import ConfigFile from 'sb-config-file'
-import FS from 'sb-fs'
 import Command from '../command'
 import { RepoManError, CONFIG_FILE_NAME } from '../helpers'
 
@@ -11,18 +11,18 @@ export default class InitCommand extends Command {
   description = 'Add a .repoman.json config file to import configurations from'
 
   async run() {
-    // TODO: Use getCurrentProject() here
-    if (await FS.exists(CONFIG_FILE_NAME)) {
+    const project = await this.getCurrentProject()
+    const configPath = Path.join(project.path, CONFIG_FILE_NAME)
+    if (await FS.exists(configPath)) {
       throw new RepoManError('.repoman.json already exists')
     }
 
     this.log('Config source? (any git repo, or Github shorthand)')
     const config = await this.helpers.prompt.input(':')
 
-    const configFilePath = Path.join(process.cwd(), CONFIG_FILE_NAME)
-    const configFile = await ConfigFile.get(configFilePath)
+    const configFile = await ConfigFile.get(configPath)
     await configFile.set('configurations', [config])
 
-    this.log(`Created ${CONFIG_FILE_NAME}`)
+    this.log(`Created configuration for ${this.helpers.tildify(project.path)}`)
   }
 }
