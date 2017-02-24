e7d52690aa52108c51bab328cd89e26f23693010
Steel Brain <i@steelbrain.me>
2017-02-24 05:03:54 +0500

    :new: Add --parallel to repoman exec

--- src/commands/ExecCommand.js
+++ src/commands/ExecCommand.js
@@ -8,6 +8,7 @@ export default class ExecCommand extends Command {
   options = [
     ['--scope <pattern>', 'Limit to packages that match comma separated pattern (eg package-name or org/repo or org/repo/package-name or org/* or *)'],
     ['--ignore <pattern>', 'Ignore packages that match pattern (eg package-name or org/repo or org/repo/package-name or org/*)'],
+    ['--parallel', 'Execute tasks in parallel', false],
   ]
 
   async run(options: Object, command: string, parameters: Array<string>) {
@@ -21,11 +22,18 @@ export default class ExecCommand extends Command {
       packages = packages.filter(i => ignored.indexOf(i) === -1)
     }
 
-    for (const pkg of packages) {
-      await this.spawn(command, parameters, {
+    if (options.parallel) {
+      await Promise.all(packages.map(pkg => this.spawn(command, parameters, {
         cwd: pkg.path,
         stdio: ['inherit', 'inherit', 'inherit'],
-      })
+      })))
+    } else {
+      for (const pkg of packages) {
+        await this.spawn(command, parameters, {
+          cwd: pkg.path,
+          stdio: ['inherit', 'inherit', 'inherit'],
+        })
+      }
     }
   }
 }
