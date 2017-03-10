3dc8641f54b6d68788232ac80cf1d3067e4fa480
Steel Brain <i@steelbrain.me>
2017-03-10 06:41:09 +0500

    :minibus: Make status command use scope instead of orgName to filter

--- src/commands/StatusCommand.js
+++ src/commands/StatusCommand.js
@@ -4,25 +4,27 @@ import Command from '../command'
 import type { Project, Package, RepositoryState } from '../types'
 
 export default class StatusCommand extends Command {
-  name = 'status [org]'
+  name = 'status'
   description = 'Get status of your projects'
   options = [
     ['--packages', 'Show status of packages instead of repositories'],
+    ['--scope <pattern>', 'Limit to results to packages or repositories that match the given pattern'],
   ]
 
-  async run(options: Object, orgName: ?string) {
+  async run(options: Object) {
     if (options.packages) {
-      await this.statusPackages(options, orgName)
+      await this.statusPackages(options)
     } else {
-      await this.statusRepositories(options, orgName)
+      await this.statusRepositories(options)
     }
   }
-  async statusPackages(options: Object, orgName: ?string) {
+  async statusPackages(options: Object) {
     const table = new this.helpers.Table({ head: ['project', 'changes', 'branch', 'path', 'npm'] })
     let packages = await this.getAllPackages()
-    if (orgName) {
-      packages = packages.filter(p => p.project.org === orgName)
+    if (options.scope) {
+      packages = this.matchPackages(packages, this.helpers.split(options.scope, ','))
     }
+
     // TODO: show a loader or something when this is happening
     const packagesRemote = await Promise.all(packages.map(pkg => this.getNodePackageState(pkg)))
     const repositories = await Promise.all(packages.map(pkg => this.getRepositoryState(pkg.project)))
@@ -32,9 +34,13 @@ export default class StatusCommand extends Command {
     }
     this.log(table.show())
   }
-  async statusRepositories(options: Object, orgName: ?string) {
+  async statusRepositories(options: Object) {
     const table = new this.helpers.Table({ head: ['project', 'changes', 'branch', 'path'] })
-    const projects = await this.getProjects(orgName)
+    let projects = await this.getProjects()
+    if (options.scope) {
+      projects = this.matchProjects(projects, this.helpers.split(options.scope, ','))
+    }
+
     const repositories = await Promise.all(projects.map(project => this.getRepositoryState(project)))
 
     for (let i = 0, length = projects.length; i < length; i++) {
